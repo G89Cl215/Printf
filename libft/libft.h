3b14ac2fdb36b8e2abccae42a342706a7f6f2293
Thomas GOUEDARD <tgouedar@e2r1p5.42.fr>
2018-12-14 16:23:43 +0100

    Otez ces objets de ma vue

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tgouedar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 18:40:46 by tgouedar          #+#    #+#             */
-/*   Updated: 2018/12/09 12:31:17 by tgouedar         ###   ########.fr       */
+/*   Updated: 2018/12/14 03:17:28 by tgouedar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,6 +56,7 @@ int					ft_isalnum(int c);
 int					ft_isascii(int c);
 int					ft_isprint(int c);
 int					ft_isin(const char c, const char *charset);
+size_t				ft_indice(char c, char *charset);
 int					ft_toupper(int c);
 int					ft_tolower(int c);
 void				*ft_memalloc(size_t size);
@@ -87,6 +88,8 @@ void				ft_putnbr_base(int n, char *base);
 void				ft_lstadd(t_list **alst, t_list *n);
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstfree(t_list **alst);
+void				ft_lstfreeone(t_list **alst);
 t_list				*ft_lstnew(void const *content, size_t content_size);
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
